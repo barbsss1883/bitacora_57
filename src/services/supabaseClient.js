19be300d777b81a01413b9a0be6859fc0ec3f52d
barbsss1883 <luiseguzman1883@gmail.com>
2026-05-12 23:56:16 -0600

    fix: supabase key + expo-updates + codemagic CI

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://sbpyojhuihdpzxgrgmid.supabase.co'
-const supabaseKey = 'sb_publishable_1Us4K-L7oNn7YeMtXCihpA_Ej1a9eQg'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicHlvamh1aWhkcHp4Z3JnbWlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3MTM4MjEsImV4cCI6MjA5MjI4OTgyMX0.o_lux8lfEzkFAQMHJPa6WamZOzxvrBSHmU140FM48jQ'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
